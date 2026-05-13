5ba9454f206e1ac4ed2723c74455aa7ee023b8b3
Casey Class <casey.class@sjsu.edu>
2026-05-12 23:05:52 -0700

    configured dependencies in pom.xml changed to my db password

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/Team9LibSys?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "ne83De-JVui";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
